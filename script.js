2e0a1cdbe6be3281b0bf9caa14a969dd3ae632a7
pmoore195 <p.moore195@gmail.com>
2021-02-03 15:19:58 -0500

    fixed bug that caused last option in message components to be impossible

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const message = {
     activities: ['do some yoga together', 'watch a movie together', 'silently scroll through your phones while holding hands', 'have loud sex', 'each read a book in a language you don\'t understand', 'create a business', 'invest in the stock market', 'have a picnic', 'look for treasure', 'write a play', 'adopt a dog'],
     endingIdeas: ['kiss them on the cheek', 'block them on social media', 'propose', 'ask them what their name is "because you forgot and were embarassed"', 'burst into tears and run away', 'tell them you had a good time but you\'re not really feeling a connection'],
     randPlace() {
-        let randIndex = Math.floor(Math.random() * (this.places.length - 1));
+        let randIndex = Math.floor(Math.random() * (this.places.length));
         return this.places[randIndex]
     },
     randActivity() {
-        let randIndex = Math.floor(Math.random() * (this.activities.length - 1));
+        let randIndex = Math.floor(Math.random() * (this.activities.length));
         return this.activities[randIndex]
     },
     randEnding() {
-        let randIndex = Math.floor(Math.random() * (this.endingIdeas.length - 1));
+        let randIndex = Math.floor(Math.random() * (this.endingIdeas.length));
         return this.endingIdeas[randIndex]
     }
 };
