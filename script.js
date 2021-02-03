0e90b108ea7fe19c8836308e40c6aae2d1a23dae
pmoore195 <p.moore195@gmail.com>
2021-02-03 14:40:42 -0500

    fixed randPlace() and cleaned up some message components

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const message = {
     places: ['a movie theater', 'a local park', 'a national park', 'your parent\'s house', 'their parent\'s house', 'an abandoned mine', 'an active mine', 'a skate park', 'the fey wild', 'the forgotten realms', 'somewhere quiet', 'the library', 'the zoo'],
-    activities: ['do some yoga together', 'watch a movie together', 'silently scroll through your phones while holding hands', 'have loud sex', 'each read a book', 'create a business', 'invest in the stock market', 'have a picnic', 'look for treasure', 'write a play', 'adopt a dog'],
-    endingIdeas: ['kiss them on the cheek', 'go to the bathroom and climb out the window', 'block them on social media', 'propose', 'ask them what their name is "because you forgot and were embarassed"', 'burst into tears and run away', 'tell them you had a good time but you\'re not really feeling a connection'],
+    activities: ['do some yoga together', 'watch a movie together', 'silently scroll through your phones while holding hands', 'have loud sex', 'each read a book in a language you don\'t understand', 'create a business', 'invest in the stock market', 'have a picnic', 'look for treasure', 'write a play', 'adopt a dog'],
+    endingIdeas: ['kiss them on the cheek', 'block them on social media', 'propose', 'ask them what their name is "because you forgot and were embarassed"', 'burst into tears and run away', 'tell them you had a good time but you\'re not really feeling a connection'],
     randPlace() {
-        let randIndex = Math.floor(Math.random * (this.places.length - 1));
+        let randIndex = Math.floor(Math.random() * (this.places.length - 1));
         return this.places[randIndex]
     },
     randActivity() {
